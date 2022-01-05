c4614f86addbe5927ec38897cb50f046f2b751b9
Miguel Tavares <miguel.pereira.515@gmail.com>
2022-01-05 23:02:27 +0000

    update

---
+++ Miguel/prepro.py
@@ -0,0 +1,147 @@
+import os
+import dill
+import click
+import logging
+
+import h5py
+import keras
+import numpy as np
+import keras.applications
+
+from tqdm import tqdm
+
+resolution = 128
+
+@click.command()
+@click.option(
+    '-ds', 
+    '--dataset-path', 
+    default='dogs-vs-cats-resnet', 
+    type=click.Path(exists=True), 
+    help='Path for your Image Dataset'
+)
+@click.option(
+    '-df',
+    '--make-datafiles',
+    is_flag=True,
+    help='Flag for converting dataset into h5 files'
+)
+def main(dataset_path, make_datafiles):
+    LOG_FORMAT = '%(levelname)s %(message)s'
+    logging.basicConfig(format=LOG_FORMAT, level='INFO')
+    #augment_images(dataset_path)
+    #move_images(dataset_path)
+    if make_datafiles:
+        sets = ['train', 'val', 'test']
+        for set_name in sets:
+            process_image_dataset(dataset_path='{}/{}'
+                .format(dataset_path, set_name), set_name=set_name)
+    logging.info('Done preprocessing!')
+
+"""
+Augment Images
+"""
+def augment_images(dataset_path):
+    categories = os.listdir('{}/train'.format(dataset_path))
+
+    datagen = keras.preprocessing.image.ImageDataGenerator(
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode='nearest')
+
+    for category in tqdm(categories, total=len(categories), desc='Augmenting Images'):
+        num_images = len(os.listdir('{}/train/{}'.format(dataset_path, category)))
+        if num_images < 800:
+            images_to_augment = os.listdir('{}/train/{}'.format(dataset_path, category))
+            num_augments_per_image = (800 - num_images) / num_images
+            if num_augments_per_image == 0:
+                images_to_augment = np.random.choice(images_to_augment, (800 - num_images))
+                num_augments_per_image = 1
+            for image_path in images_to_augment:
+                image = keras.preprocessing.image.load_img('{}/train/{}/{}'
+                        .format(dataset_path, category, image_path))
+                x = keras.preprocessing.image.img_to_array(image)
+                x = x.reshape((1,) + x.shape)
+                i = 0
+                for _ in datagen.flow(x, batch_size=1,
+                    save_to_dir='{}/train/{}'.format(dataset_path, category), 
+                    save_prefix=image_path[:-4], save_format='jpg'):
+                    i += 1
+                    if i > num_augments_per_image:
+                        break
+    logging.info('Done augmenting images!')
+
+"""
+Move Images into [train, val, test] sets
+"""
+def move_images(dataset_path):
+    categories = os.listdir('{}/train'.format(dataset_path))
+
+    for category in tqdm(categories, total=len(categories), desc='Moving Images'):
+        #os.mkdir('{}/val/{}'.format(dataset_path, category))
+        category_images = os.listdir('{}/train/{}'.format(dataset_path, category))
+        random_val_images = np.random.choice(category_images, 20, replace=False)
+        for image_path in random_val_images:
+            os.rename('{}/train/{}/{}'.format(dataset_path, category, image_path),
+                    '{}/val/{}/{}'.format(dataset_path, category, image_path))
+
+        #os.mkdir('{}/test/{}'.format(dataset_path, category))
+        #category_images = os.listdir('{}/train/{}'.format(dataset_path, category))
+        #random_test_images = np.random.choice(category_images, 20, replace=False)
+        #for image_path in random_test_images:
+            #os.rename('{}/train/{}/{}'.format(dataset_path, category, image_path),
+                    #'{}/test/{}/{}'.format(dataset_path, category, image_path))
+    logging.info('Done moving images!')
+
+"""
+Preprocess Image
+"""
+def preprocess_image(image):
+    x = keras.preprocessing.image.img_to_array(image)
+    x = np.expand_dims(x, axis=0)
+    return keras.applications.imagenet_utils.preprocess_input(x)[0]
+
+"""
+Process Image Dataset into h5 data files
+"""
+def process_image_dataset(dataset_path=None, set_name='train'):
+    if dataset_path is None:
+        raise UserWarning('Dataset path should not be None!')
+
+    X, y = [], []
+    categories = os.listdir(dataset_path)
+    # read images from dataset dir
+    for category in tqdm(categories, total=len(categories), desc='Processing {} images'.format(set_name)):
+        image_label = categories.index(category)
+        for image_path in os.listdir('{}/{}'.format(dataset_path, category)):
+            image = keras.preprocessing.image.load_img('{}/{}/{}'
+                .format(dataset_path, category, image_path), 
+                target_size=(resolution, resolution))
+            image_pr = preprocess_image(image)
+            X.append(image_pr)
+            y.append(image_label)
+
+    # convert to desired format
+    X = np.array(X)
+    y = keras.utils.np_utils.to_categorical(y, num_classes=len(categories))
+    
+    logging.info('{} features shape: {}'.format(set_name, X.shape))
+    logging.info('{} targets shape: {}'.format(set_name, y.shape))
+
+    #write final
+    logging.info('Writing preprocessed {} data to files'.format(set_name))
+    train_file = h5py.File('datasets/{}_data.h5'.format(set_name), "w")
+    train_file.create_dataset('X', data=X)
+    train_file.create_dataset('y', data=y)
+
+    if not os.path.exists('datasets/classes.dill'):
+        logging.info('Writing classes to file')
+        with open('datasets/classes.dill', 'wb') as file:
+            dill.dump(categories, file)
+
+if __name__ == '__main__':
+    main()
---
+++ Miguel/train.py
@@ -0,0 +1,182 @@
+import os
+import click
+import logging
+
+import keras
+import numpy as np
+import keras.backend as K
+
+from model import create_model
+
+import tensorflow as tf
+
+
+
+
+
+K.set_image_data_format('channels_last')
+
+"""
+Train Model [optional args]
+"""
+@click.command(name='Training Configuration')
+@click.option(
+    '-lr', 
+    '--learning-rate', 
+    default=0.005, 
+    help='Learning rate for minimizing loss during training'
+)
+@click.option(
+    '-bz',
+    '--batch-size',
+    default=32,
+    help='Batch size of minibatches to use during training'
+)
+@click.option(
+    '-ne', 
+    '--num-epochs', 
+    default=50, 
+    help='Number of epochs for training model'
+)
+@click.option(
+    '-se',
+    '--save-every',
+    default=1,
+    help='Epoch interval to save model checkpoints during training'
+)
+@click.option(
+    '-tb',
+    '--tensorboard-vis',
+    is_flag=True,
+    help='Flag for TensorBoard Visualization'
+)
+@click.option(
+    '-ps',
+    '--print-summary',
+    is_flag=True,
+    help='Flag for printing summary of the model'
+)
+def train(learning_rate, batch_size, num_epochs, save_every, tensorboard_vis, print_summary):
+    setup_paths()
+
+    datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+    get_gen = lambda x: datagen.flow_from_directory(
+        'dogs-vs-cats-resnet/{}'.format(x),
+        target_size=(64, 64),
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
+
+    # generator objects
+    train_generator = get_gen('train')
+    val_generator = get_gen('val')
+    test_generator = get_gen('test')
+
+    if os.path.exists('models/resnet50.h5'):
+        # load model
+        logging.info('loading pre-trained model')
+        resnet50 = keras.models.load_model('models/resnet50.h5')
+    else:
+        # create model
+        logging.info('creating model')
+        resnet50 = create_model(input_shape=(64, 64, 3), classes=102)
+    
+    optimizer = keras.optimizers.Adam(learning_rate)
+    resnet50.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    if print_summary:
+        resnet50.summary()
+
+    callbacks = configure_callbacks(save_every, tensorboard_vis)
+
+    # train model
+    logging.info('training model')
+    resnet50.fit_generator(
+        train_generator,
+        steps_per_epoch=72000//batch_size,
+        epochs=num_epochs,
+        verbose=1,
+        validation_data=val_generator,
+        validation_steps=2200//batch_size,
+        shuffle=True,
+        callbacks=callbacks
+    )
+    # save model
+    logging.info('Saving trained model to `models/resnet50.h5`')
+    resnet50.save('models/resnet50.h5')
+
+    # evaluate model
+    logging.info('evaluating model')
+    preds = resnet50.evaluate_generator(
+        test_generator,
+        steps=2200//batch_size,
+        verbose=1
+    )
+    logging.info('test loss: {:.4f} - test acc: {:.4f}'.format(preds[0], preds[1]))
+
+    keras.utils.plot_model(resnet50, to_file='models/resnet50.png')
+
+"""
+Configure Callbacks for Training
+"""
+def configure_callbacks(save_every=1, tensorboard_vis=False):
+    # checkpoint models only when `val_loss` impoves
+    saver = keras.callbacks.ModelCheckpoint(
+        'models/ckpts/model.ckpt',
+        monitor='val_loss',
+        save_best_only=True,
+        period=save_every,
+        verbose=1
+    )
+    
+    # reduce LR when `val_loss` plateaus
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(
+        monitor='val_loss',
+        factor=0.1,
+        patience=5,
+        verbose=1,
+        min_lr=1e-10
+    )
+
+    # early stopping when `val_loss` stops improving
+    early_stopper = keras.callbacks.EarlyStopping(
+        monitor='val_loss', 
+        min_delta=0, 
+        patience=10, 
+        verbose=1
+    )
+
+    callbacks = [saver, reduce_lr, early_stopper]
+
+    if tensorboard_vis:
+        # tensorboard visualization callback
+        tensorboard_cb = keras.callbacks.TensorBoard(
+            log_dir='./logs',
+            write_graph=True,
+            write_images=True
+        )
+        callbacks.append(tensorboard_cb)
+    
+    return callbacks
+
+def setup_paths():
+    if not os.path.isdir('models/ckpts'):
+        if not os.path.isdir('models'):
+            os.mkdir('models')
+        os.mkdir('models/ckpts')
+
+def main():
+    LOG_FORMAT = '%(levelname)s %(message)s'
+    logging.basicConfig(
+        format=LOG_FORMAT, 
+        level='INFO'
+    )
+
+    try:
+        train()
+    except KeyboardInterrupt:
+        print('EXIT')
+
+if __name__ == '__main__':
+    main()
